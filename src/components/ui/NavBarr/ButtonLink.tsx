0535fd57b0ca7628c4c1dbad10bc632e713f1636
SENA <ronaldzamoradev@gmail.com>
2023-07-13 14:50:09 -0500

    fix: scroll animation

--- src/components/ui/NavBarr/ButtonLink.tsx
+++ src/components/ui/NavBarr/ButtonLink.tsx
@@ -1,10 +1,9 @@
-import Link from 'next/link'
 import { ReactNode } from 'react'
 
 export function ButtonLink ({ children, href }: { children: ReactNode, href: string }) {
   return (
-    <Link href={href} className='[@media(min-width:800px)]:text-base hover:bg-[#2A2A2A] p-2 rounded-md transition-all hover:text-red-600'>
+    <a href={href} className='[@media(min-width:800px)]:text-base hover:bg-[#2A2A2A] p-2 rounded-md transition-all hover:text-red-600'>
       {children}
-    </Link>
+    </a>
   )
 }
